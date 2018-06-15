5c2117fb72f0e1a4752a2eaad47f820048167277
Neelesh Bizoara <bizoaraneelesh@gmail.com>
2018-06-15 16:48:58 +0530

    Chnage gretting

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 app.get('/', (req, res) => {
-    res.send( {hi: 'there'} );
+    res.send( {bye: 'buddy'} );
 });
 
